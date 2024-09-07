96abd3c179c674a7a183bca1a4b95db8209190a0
drashtipatell18 <drashti.kalathiyainfotech@gmail.com>
2024-09-07 12:51:33 +0530

    add route web auth

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Auth; 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
